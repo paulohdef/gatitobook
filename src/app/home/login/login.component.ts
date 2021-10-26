525a385bbaea5e1090213e02dacd952c942fb7a7
Paulo de Freitas <paulohdef1@gmail.com>
2021-10-26 11:57:28 -0300

    faltou implementar service para o component lista

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,28 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  usuario = '';
+  senha = '';
+
+  constructor(private authService: AutenticacaoService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.autenticar(this.usuario,this.senha).subscribe(()=> {
+      console.log('Autenticado com sucesso');
+    }, (error)=>{
+      alert("Usuário ou senha inválido");
+      console.log(error);
+    });
+  }
+
+}
