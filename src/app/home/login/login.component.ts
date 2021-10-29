e6fb5d8d0afb35868e992b33bccfbbc7a676f7e5
Paulo de Freitas <paulohdef1@gmail.com>
2021-10-29 12:13:28 -0300

    implementação reactive forms

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,5 +1,6 @@
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,19 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(
+    private authService: AutenticacaoService,
+    private router:Router
+  ) {}
 
   ngOnInit(): void {
   }
 
   login(){
     this.authService.autenticar(this.usuario,this.senha).subscribe(()=> {
-      console.log('Autenticado com sucesso');
-    }, (error)=>{
+      this.router.navigate(['animais']);
+    },
+    (error)=>{
       alert("Usuário ou senha inválido");
       console.log(error);
     });
